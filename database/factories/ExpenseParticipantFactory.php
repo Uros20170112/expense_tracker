90a09ae05ae0919df67adafa341d45fbf3ffb168
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2024-06-20 23:23:39 +0200

    created ExpenseParticipant and payment model and factory

--- database/factories/ExpenseParticipantFactory.php
+++ database/factories/ExpenseParticipantFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ExpenseParticipant>
  */
-class CategoryFactory extends Factory
+class ExpenseParticipantFactory extends Factory
 {
     /**
      * Define the model's default state.
