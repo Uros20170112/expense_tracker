b54667985c6d4c2728f1b0e827b15f87b8755a73
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2024-06-21 22:04:40 +0200

    added user id to expense

--- database/migrations/2024_06_21_113637_add_user_id_to_expenses.php
+++ database/migrations/2024_06_21_113637_add_user_id_to_expenses.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('expenses', function (Blueprint $table) {
-            $table->foreignId('paid_by')->references('id')->on('users')->onDelete('cascade');
+            // $table->foreignId('paid_by')->references('id')->on('users')->onDelete('cascade');
             // $table->unsignedBigInteger('paid_by')->nullable()->after('id');
-            // $table->foreign('paid_by')->references('id')->on('users')->onDelete('cascade');
+            // $table->foreign('paid_by')->after('id')->nullable()->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -24,7 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('expenses', function (Blueprint $table) {
-            //
+            $table->dropForeign(['paid_by']);
+            $table->dropColumn('paid_by');
         });
     }
 };
