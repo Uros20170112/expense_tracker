9ae9ca0409c0bee13d8878043a96861e05a6548a
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2024-06-23 19:29:39 +0200

    Created Navbar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-xl navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarBasic"
+          aria-controls="navbarBasic"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarBasic">
+          <ul className="navbar-nav me-auto mb-2 mb-xl-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            ></input>
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
